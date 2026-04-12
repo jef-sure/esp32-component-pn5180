63fd2925d2071192f2572e1bde31f75720c8e46c
Garag <garag@everwait.de>
2026-04-12 18:04:51 +0200

    reorganized examples

--- src/pn5180.c
+++ src/pn5180.c
@@ -2,6 +2,7 @@
 #include "esp_heap_caps.h"
 #include "esp_log.h"
 #include "esp_timer.h"
+#include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "pn5180-internal.h"
 #include <inttypes.h>
